0bb71289377822aa9cbd5cff70a1c08669a26669
Mehar-IT <hamzatariquearain@gmail.com>
2021-05-21 22:58:58 +0500

    added search bar

--- lib/main.dart
+++ lib/main.dart
@@ -25,19 +25,68 @@ class SearchBar extends StatelessWidget {
   }
 }
 
-class MyList extends StatelessWidget {
+class MyList extends StatefulWidget {
+  @override
+  _MyListState createState() => _MyListState();
+}
+
+class _MyListState extends State<MyList> {
+  List<String> item;
+  List<String> data = List.generate(50, (index) => 'item ${index + 1}');
+
+  @override
+  void initState() {
+    item = data;
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
+        MyTextField(
+          onchanged: (query) {
+            setState(() {
+              item = data.where((element) {
+                final String lower = element.toLowerCase();
+                final String upper = element.toUpperCase();
+
+                return lower.contains(query) || upper.contains(query);
+              }).toList();
+            });
+          },
+        ),
         Expanded(
           child: ListView.builder(
-              itemCount: 30,
+              itemCount: item.length,
               itemBuilder: (context, index) => ListTile(
-                    title: Center(child: Text('hamza $index')),
+                    title: Center(child: Text('${item[index]}')),
                   )),
         ),
       ],
+    );
+  }
+}
+
+class MyTextField extends StatelessWidget {
+  final ValueSetter onchanged;
+  const MyTextField({
+    @required this.onchanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        onChanged: onchanged,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Search',
+          hintText: 'Enter an item name',
+        ),
+      ),
     );
   }
 }
